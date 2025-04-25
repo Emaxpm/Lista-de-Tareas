ce20c9aaa08351599f162038834173a72dbb652d
Emaxpm <emmapm08@gmail.com>
2025-04-25 10:17:00 -0600

    first commit

---
+++ components/ui/TareaTarjeta.tsx
@@ -0,0 +1,60 @@
+"use client"
+import React, { useState } from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { CheckIcon } from 'lucide-react'
+import { Separator } from "@/components/ui/separator"
+
+
+
+type Props = {
+    titulo: string;
+    desc: string;
+    date: Date;
+    isCompleted: boolean;
+}
+
+const TareaTarjeta = ({ titulo, desc, date, isCompleted }: Props) => {
+
+    const [completed, setCompleted] = useState(isCompleted)
+
+    const handleClick = () =>{
+       setCompleted((prevState) => !prevState)
+    }
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle  className='text-xl text-gray-800'>
+                    {titulo}
+                </CardTitle>
+            </CardHeader>
+            <Separator />
+            <CardContent>
+                <p className='text-sm textgray-600'> {desc} </p>
+            </CardContent>
+            <Separator />
+            <CardFooter className='flex justify-between'>
+                <p className='text-sm textgray-600'> {date.toDateString()} </p>
+
+                {completed ? (
+                    <CheckIcon className='text-green-400'/>
+                ) : (<Button variant="outline" onClick={handleClick}>Completar</Button>)
+                }
+
+                
+
+            </CardFooter>
+        </Card>
+
+    )
+}
+
+export default TareaTarjeta
---
+++ contants/index.ts
@@ -0,0 +1,29 @@
+export const  tareas = [
+    {
+        titulo: "Comprar comestibles",
+        desc: "Comprar alimentos para la semana: arroz, frijoles,aceite.",
+        date: new Date("2024-06-02T00:00:00.000Z"),
+        isCompleted: false
+    },
+
+    {
+        titulo: "Preparar presentasión",
+        desc: "Crea y ensaya la presentacion para el lunes.",
+        date: new Date("2024-09-05T00:00:00.000Z"),
+        isCompleted: false
+    },
+
+    {
+        titulo: "Enviar reporte mensual",
+        desc: "Completa y envia el reporte financiero del mes",
+        date: new Date("2024-05-09T00:00:00.000Z"),
+        isCompleted: true
+    },
+
+    {
+        titulo: "Leer libro",
+        desc: "Leer capitulo 3 de /Habitos/ Atomicos",
+        date: new Date("2024-06-02T00:00:00.000Z"),
+        isCompleted: false
+    },
+]
